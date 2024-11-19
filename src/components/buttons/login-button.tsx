aa31b8b27cefc0800ef3ffb77dc64d29ad407549
Byron Motoche <byron.motoche@okta.com>
2024-11-19 17:22:27 -0500

    implement user authentication

---
+++ src/components/buttons/login-button.tsx
@@ -0,0 +1,7 @@
+export const LoginButton = () => {
+  return (
+    <a className="button__login" href="/auth/login?returnTo=/profile">
+      Log In
+    </a>
+  );
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,27 @@
 import type { NextRequest } from "next/server";
 
 import { auth0 } from "./lib/auth0";
+import { NextResponse } from "next/server";
+
+const protectedRoutes = new Set(["/profile", "/protected", "/admin"]);
 
 export async function middleware(request: NextRequest) {
-  return await auth0.middleware(request);
+  const authResponse = await auth0.middleware(request);
+
+  if (request.nextUrl.pathname.startsWith("/auth")) {
+    return authResponse;
+  }
+
+  const session = await auth0.getSession();
+
+  if (protectedRoutes.has(request.nextUrl.pathname) && !session) {
+    const loginURL = new URL("/auth/login", process.env.APP_BASE_URL);
+    loginURL.searchParams.append("returnTo", request.nextUrl.pathname);
+
+    return NextResponse.redirect(loginURL);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
